e164bae7c1937ae5ab9a0d59ccca3f57e3b35abe
GPonomarev <pvg9kz@gmail.com>
2023-01-11 17:02:48 +0600

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const firstDigit = Math.abs(n % 10);
+    const secondDigit = (Math.abs(n % 100) / 10) | 0;
+    const thirdDigit = (Math.abs(n) / 100) | 0;
+    return firstDigit * 100 + secondDigit * 10 + thirdDigit;
+};
